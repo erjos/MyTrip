0a5f8ab9c2f3ffdd6077400ceed394b9ced46cc1
Joseph, Ethan <Ethan.Joseph@tdameritrade.com>
2017-05-08 22:05:36 -0400

    Initial Commit

---
+++ MyTrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTrip
+//
+//  Created by Joseph, Ethan on 5/8/17.
+//  Copyright © 2017 Joseph, Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
