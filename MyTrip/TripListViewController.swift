1ea1ad168df5bf9ad8830072f81d61f3373600b6
Joseph <Ethan.Joseph@tdameritrade.com>
2017-06-21 18:07:21 -0400

    tripsListVC should now load up trips that are stored in core data

--- MyTrip/TripListViewController.swift
+++ MyTrip/TripListViewController.swift
@@ -1,23 +1,44 @@
 import UIKit
+import CoreData
 
 class TripListViewController: UIViewController {
-
+    
+    var trips : [NSManagedObject] = []
+    
     @IBOutlet weak var tripListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tripListTableView.dataSource = self
         tripListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Trip")
+        do {
+            trips = try managedContext.fetch(fetchRequest)
+            self.tripListTableView.reloadData()
+        } catch let error as NSError{
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 }
 
 extension TripListViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //will return number of trips
-        return 5
+        return trips.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let trip = trips[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = trip.value(forKey: "name") as? String
         return cell
     }
 }
